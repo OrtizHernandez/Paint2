035ba38cd59a6d0a63985183331c74a9b9fe2a63
Adan Ortiz <adanortiz9876@gmail.com>
2024-03-01 21:04:13 -0600

    Version 1 Adicion de una userGUI atraves de un form de intelli

---
+++ src/main/java/org/example/Visual/PaintGUI.java
@@ -0,0 +1,28 @@
+package org.example.Visual;
+
+import javax.swing.*;
+
+public class PaintGUI {
+    private JPanel panelGeneral;
+    private JPanel panelNorte;
+    private JPanel panelSur;
+    private JPanel panelCenter;
+    private JButton btnLimpiar;
+    private JButton btonGuardar;
+    private JLabel lblCoordenadas;
+    private JSlider sliderPincel;
+    private JButton btnColorNegro;
+    private JLabel lblFiguras;
+    private JButton btnRectangulo;
+    private JButton btnCirculo;
+    private JButton btnLinea;
+    private JButton btnCuadrado;
+    private JPanel panelOeste;
+    private JPanel panelEste;
+    private JLabel lblTitulo;
+    private JLabel lblPincel;
+    private JLabel lblColores;
+    private JButton btnColorAzul;
+    private JButton btnColorRojo;
+    private JButton btnColorVerde;
+}
